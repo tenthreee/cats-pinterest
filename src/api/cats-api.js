4da01f2414b27980d3f32f0850b0813fc44ec901
tenthreee <lena@htmlacademy.ru>
2022-05-31 19:15:03 +0300

    Updates

--- src/api/cats-api.js
+++
@@ -1,6 +0,0 @@
-function getCats() {
-    return fetch('https://api.thecatapi.com/v1/images/search?limit=15&mime_types=jpg,png')
-            .then((response) => response.json());
-}
-
-export default getCats;
